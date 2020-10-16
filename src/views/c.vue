5fb7c07ae7288dd12cb80a31038505f1dcfdef9c
dancingCode-zx <3169396052@qq.com>
2020-10-16 16:27:30 +0800

    ccc

---
+++ src/views/c.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>ccc</div>
+</template>
+
+<script>
+export default {
+  components:{},
+  props:[],
+  data(){
+    return {
+    }
+  },
+}
+</script>
+<style lang=''>
+</style>
